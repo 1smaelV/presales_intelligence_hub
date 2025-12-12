f2ffa22fec446e2209f8fc92dede18776133ee37
Ismael Villegas <ismael.villegasdiaz@concentrix.com>
2025-12-11 23:07:11 -0600

    Create vite-env.d.ts

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly BRIEF_MODEL?: string;
+  readonly OPENAI_MODEL?: string;
+  readonly AI_ANALYZER_MODEL?: string;
+  readonly OPENAI_API_KEY?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
